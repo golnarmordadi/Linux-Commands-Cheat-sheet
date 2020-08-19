db5a93cea7eb50e9778668f37de2d2519199913e
Hakimeh Mordadi <hakimehmordadi@gmail.com>
2020-08-19 23:13:12 +0430

    Update BashScrip.sh

--- BashScrip.sh
+++ BashScrip.sh
@@ -3,4 +3,49 @@
 # SHORTCUTS and HISTORY
 ##############################################################################
 
-`Ctrl+Shift+N`    >. Open Terminal
+Ctrl+Shift+N  #Open Terminal
+Ctrl+Shift+Q  #Close Treminal
+Ctrl+Shift+T  #Add Tab
+Ctrl+Shift+W  #Delete Tab
+Ctrl ++       #Zoom In
+Ctrl --       #Zoom Out
+Ctrl H        #show hidden files on a folder
+
+##############################################################################
+# Internal Commands
+##############################################################################
+
+echo         #Print
+cal          #Calander
+date         #date and hour of today
+history      #history of all commands
+!1           #`1` is line number `after history command with line number and !` we can run that line 
+histoery -c  #empty history of terminal
+date 2012    #all month of 20120
+comment      ##
+shebang      #!
+$?           #`echo $?` = 1(command done), 0(command failed)
+free         #shows memory info
+pwd          #current directory
+cd ~/        #enter into directory
+cd /         #enter into root directory
+cd ..        #existing from directory
+tty          #showing the path of current open terminal
+htop         #shows all processes list
+$$           #will give you the processid of the script
+$EUID        #`echo $EUID` effective user ID describes the user whose file access permissions are used by the process
+$RUID        #`echo $RUID` real user ID identifies the user who created the process
+$UID         #`echo $UID`  is a unique identification number assigned to every user present in a Linux system
+$GID         #`echo $GID'  group identifier
+$PATH        #`echo $PATH` the current command directory
+$PID         #`echo $PID`  non-negative processid
+
+##############################################################################
+# File Commands
+##############################################################################
+
+
+                *-------------------*
+
+
+
